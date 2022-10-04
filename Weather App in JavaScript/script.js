bff649e54fea749fe89577acd5fc978e98590c3f
Aishuvishwakarma <vishwakarmaa949@gmail.com>
2022-10-04 12:18:05 +0530

    key updated

--- Weather App in JavaScript/script.js
+++ Weather App in JavaScript/script.js
@@ -25,13 +25,13 @@ locationBtn.addEventListener("click", () =>{
 });
 
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5d6b16796d2dd716380d9260d2e8fd38`;
     fetchData();
 }
 
 function onSuccess(position){
     const {latitude, longitude} = position.coords; // getting lat and lon of the user device from coords obj
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=5d6b16796d2dd716380d9260d2e8fd38`;
     fetchData();
 }
 
